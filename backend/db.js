0cfb01c3716910c465acf2b2c462f09aa36f3244
M Jamal Ahmad Khan <bscs24f51@namal.edu.pk>
2026-07-14 15:46:08 +0500

    Mask database password in db.js

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const poolConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Jamal543@',
+  password: process.env.DB_PASSWORD || '#########',
   database: process.env.DB_NAME || 'namalcriko',
   waitForConnections: true,
   connectionLimit: 10,
